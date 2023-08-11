a902a73981b82888df76e918777e820a16a2e4ba
abel-alx-github <abeltibeso323@gmail.com>
2023-08-10 21:01:38 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <string.h>
+/**
+*main - entry point
+*Description: to print string using write()
+*Return: Always 1 (success)
+*/
+int main(void)
+{
+const char *abel = "and that piece of art is useful" - Dora Korpar,
+2015 - 10 - 19\n";
+size_t len = strlen(abel);
+write(STDOUT_FILENO, abel, len);
+return (1);
+}
