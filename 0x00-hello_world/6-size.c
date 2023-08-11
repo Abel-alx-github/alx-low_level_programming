4a2747aad5785763a7cc7b7863530f356449b72b
abel-alx-github <abeltibeso323@gmail.com>
2023-08-10 19:27:24 -0700

    task6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main -entry level
+* Return: alwalys 0 (sucess)
+*/
+int main(void)
+{
+printf("Size of a char: %u sizeof(char)\n");
+printf("Size of an int: %u sizeof(int)\n");
+printf("Size of a long int: %u sizeof(long int)\n");
+printf("Size of a long long int:%u sizeof(long long int)\n");
+printf("Size of a float: %u sizeof(float)\n");
+return (0);
+}
