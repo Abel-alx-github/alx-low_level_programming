1340890c026144b8582c5de794e245fdbab6e7cc
abel-alx-github <abeltibeso323@gmail.com>
2023-08-11 22:42:15 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+*main -prints all possible different combinations of two digits.
+*Return: always 0 success
+*/
+int main(void)
+{
+int a;
+int b;
+for (a = 48; a < 57; a++)
+{
+for (b = 49; b < 58; b++)
+{
+if (a != b && a < b)
+{
+putchar(a);
+putchar(b);
+if (a != 56 || b != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
+
