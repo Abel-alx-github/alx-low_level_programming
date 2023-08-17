d8a14b6fa75456c087eb57d4c707aef5c6f11de8
abel-alx-github <abeltibeso323@gmail.com>
2023-08-17 04:44:15 -0700

    task 0 to 4

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+*_isupper -checks for uppercase character
+*@c: whose value is int type
+*Return: 1 if c is uppercase, otherwise 0
+*/
 int _isupper(int c)
 {
 if (c >= 65 && c <= 90)
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*mul -multiplies two integers
+*@a: int is gonna be multiplied
+*@b: int is gonna be multiplied
+*Return: (a * b)
+*/
+
+
+int mul(int a, int b)
+{
+return (a * b);
+
+}
