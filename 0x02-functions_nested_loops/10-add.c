8c273a96f09484176fd0832fa175e935602c1607
abel-alx-github <abeltibeso323@gmail.com>
2023-08-16 19:57:47 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*add -adds two integer and return result
+*@a: int to be added to b
+*@b: int to ber added to a
+*Returnn: sum of a and b
+*/
+int add(int a,  int b)
+{
+int c;
+c = a + b;
+return (c);
+}
