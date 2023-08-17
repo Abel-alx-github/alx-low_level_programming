334b457aa82516978801cb0dd05e480cfb211f15
abel-alx-github <abeltibeso323@gmail.com>
2023-08-17 08:14:28 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,14 +1,16 @@
 #include "main.h"
 /**
-*more numbers - prints 10 times the numbers, from 0 to 14
-*return:  10 times the numbers, from 0 to 14
+*more_numbers - prints 10 times the numbers, from 0 to 14
+*Return: 10 times the numbers, from 0 to 14
 */
 void more_numbers(void)
 {
 char a, b;
-for (a = 1; a <= 10; a++)
-{for (b = 0; b <= 14; b++)
-{if (b > 9)
+for (a = 0; a < 10; a++)
+{
+for (b = 0; b <= 14; b++)
+{
+if (b > 9)
 {
 _putchar((b / 10) + '0');
 }
