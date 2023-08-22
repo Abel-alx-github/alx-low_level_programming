ae96b7af8be33a6474b8974a6f63c423c38ecb3a
abel-alx-github <abeltibeso323@gmail.com>
2023-08-22 08:28:43 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+	int a;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	s--;
+	for (a = len; a > 0; a--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
+
