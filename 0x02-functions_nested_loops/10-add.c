ab9bd1f773667039bd1c94607a54f1c3fdf2970a
abel-alx-github <abeltibeso323@gmail.com>
2023-08-25 06:41:10 -0700

    TASK 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,11 +3,9 @@
 *add -adds two integer and return result
 *@a: int to be added to b
 *@b: int to ber added to a
-*Returnn: sum of a and b
+*Return: sum of a and b
 */
 int add(int a,  int b)
 {
-int c;
-c = a + b;
-return (c);
+return (a + b);
 }
