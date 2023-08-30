50015c949a534138d9fb41581a209251e3cfa729
abel-alx-github <abeltibeso323@gmail.com>
2023-08-30 00:08:42 -0700

    task 2~

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,13 +7,14 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-int i, eq = 0;
+int i;
 for (i = 0; s1[i] != '\0' && s2[i] && '\0'; i++)
 {
 	if (s1[i] == s2[i])
 	{
-	return (eq);
+	return (0);
 	}
 }
-return (s1[i] - s2[i]);
+
+	return (s1[i] - s2[i]);
 }
