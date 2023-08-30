c6bcf88130caeb11a2d349aa6043907aac36491b
abel-alx-github <abeltibeso323@gmail.com>
2023-08-30 09:50:50 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*string_toupper: function that convert lower case to upper
+*@ch: input string
+*Return: returns converted char ch
+*
+*/
+char *string_toupper(char *ch)
+{
+int i = 0;
+for (i = 0; ch[i] != '\0'; i++)
+	{
+	if (ch[i] >= 'a' && ch[i] <= 'z')
+		{
+		ch[i] = ch[i] - 32;
+		}
+
+	}
+return (ch);
+}
+
