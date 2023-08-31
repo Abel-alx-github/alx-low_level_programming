a5931a888d8d68c239a0f3f3fd8a3420d7dce662
abel-alx-github <abeltibeso323@gmail.com>
2023-08-30 21:47:21 -0700

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+**leet - function thta encode
+*@ch: input value
+*Return: returnc encoded ch
+*
+*/
+char *leet(char *ch)
+
+{
+char str1[] = "aAeEoOtTlL";
+char str2[] = "4433007711";
+int a, b;
+for (a = 0; ch[a] != '\0'; a++)
+	{
+	for (b = 0; b < 10; b++)
+		if (ch[a] == str1[b])
+			ch[a] = str2[b];
+	}
+return (ch);
+
+}
