fef8da32d8a0cff47146f14faff6ccd4b0d97e5b
abel-alx-github <abeltibeso323@gmail.com>
2023-09-14 00:36:19 -0700

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+*malloc_checked - allocate memory for new pointer
+*@b: size to be allocated the pointer
+*Return: pointer
+ */
+void *malloc_checked(unsigned int b)
+
+{
+	(void *) new;
+
+	new = malloc(b);
+
+	if (new == 0)
+		exit(98);
+
+	return (new);
+
+}
