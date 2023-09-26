6c8bfdd35a282407bfba592bca7e43112cf513b3
abel-alx-github <abeltibeso323@gmail.com>
2023-09-26 13:04:30 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *_strchr - locates a character in a string
+ *@s: string
+ *@c: character to be located
+ *Return: pointer
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s[i]);
+	}
+
+	if (s[i] == '\0')
+		return (NULL);
+
+
+}
