de173d7ae173de068d956d31285c2dcac5e56fa4
abel-alx-github <abeltibeso323@gmail.com>
2023-09-26 13:12:46 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,14 +9,14 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; *s != '\0'; s++, i++)
 	{
-		if (s[i] == c)
-			return (s[i]);
+		if (*s == c)
+			break;
 	}
 
-	if (s[i] == '\0')
-		return (NULL);
+	if (*s == '\0')
+		s = NULL;
 
-
+	return (s);
 }
