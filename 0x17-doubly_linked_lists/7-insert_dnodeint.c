4559cfe44f2d06908a15134000dde00def5ddbbb
abel-alx-github <abeltibeso323@gmail.com>
2023-11-11 08:59:46 -0800

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,19 +9,18 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *temp_1, *new = NULL;
-	unsigned int i;
+	unsigned int count, i = 1;
 
 	if (*h == NULL)
+		return (NULL);
+	count = len(*h);
+	if (idx > count)
 		return (NULL);
 	if (idx == 0)
 		new = add_dnodeint(h, n);
 	else
 	{
-		i = 1;
 		temp_1 = *h;
-		if (temp_1)
-			while (temp_1->prev)
-				temp_1 = temp_1->prev;
 		while (temp_1)
 		{
 			if (idx == i)
@@ -47,4 +46,21 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		}
 	}
 	return (new);
+}
+/**
+*len- count length or number of node
+*@h: ponter to head node
+*Return: no of node
+*/
+int len(dlistint_t *h)
+{
+	dlistint_t *temp = h;
+	int count = 0;
+
+	while (temp)
+	{
+		count++;
+		temp = temp->next;
+	}
+	return (count);
 }
