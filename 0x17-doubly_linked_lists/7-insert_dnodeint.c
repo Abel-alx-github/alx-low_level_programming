6d91446270138af0e228a9be9af66ca6f090b052
abel-alx-github <abeltibeso323@gmail.com>
2023-11-11 09:05:28 -0800

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,6 +16,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	count = len(*h);
 	if (idx > count)
 		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
 	if (idx == 0)
 		new = add_dnodeint(h, n);
 	else
@@ -29,15 +32,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 					new = add_dnodeint_end(h, n);
 				else
 				{
-					new = malloc(sizeof(dlistint_t));
-					if (new)
-					{
 						new->n = n;
 						new->next = temp_1->next;
 						new->prev = temp_1;
 						temp_1->next->prev = new;
 						temp_1->next = new;
-					}
 				}
 				break;
 			}
