e868eadacb42f811f5b3c7ff5de2d1c1e2d00a5b
abel-alx-github <abeltibeso323@gmail.com>
2023-11-12 10:48:08 -0800

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+*free_listint2 - frees list
+*@head: pointer to pointer of head
+*
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+	temp = *head;
+	while (head)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+}
