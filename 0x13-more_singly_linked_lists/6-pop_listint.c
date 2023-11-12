0c02ba12ad1c4de15ffaf615ed643cd99b7b9c16
abel-alx-github <abeltibeso323@gmail.com>
2023-11-12 11:14:58 -0800

    task 5

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,11 +9,14 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
+	int data;
 
 	if (head == NULL)
 		return (0);
 	temp = *head;
 	*head = (*head)->next;
 	temp->next = NULL;
-	return (temp->n);
+	data = temp->n;
+	free(temp);
+	return (data);
 }
