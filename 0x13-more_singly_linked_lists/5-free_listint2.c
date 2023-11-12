3dbaa97bd463ea91cdab7244284def2bc7f0bbe9
abel-alx-github <abeltibeso323@gmail.com>
2023-11-12 10:57:30 -0800

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,11 +12,11 @@ void free_listint2(listint_t **head)
 		return;
 	while (head)
 	{
-		temp = *head;
-		*head = (*head)->next;
+		temp = (*head);
+		(*head) = (*head)->next;
 		free(temp);
 		temp = NULL;
 	}
-	*head = NULL;
+	(*head) = NULL;
 }
 
