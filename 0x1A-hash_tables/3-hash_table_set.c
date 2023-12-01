8e917f69a90ea1cd73d3734fd5e882c4e541daaf
abel-alx-github <abeltibeso323@gmail.com>
2023-12-01 10:20:06 -0800

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,20 +10,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int i, idx;
 	hash_node_t *new;
-	char *cp_value, *cp_key;
+	char *cp_value;
 
-	if (ht == NULL || key == NULL || value || key)
+	if (ht == NULL || *key == "\0" || key == NULL || value || key)
 	{
 		return (0);
 	}
 
-
 	idx = key_index((const unsigned char *) key, ht->size);
 	cp_value = strdup(value);
+	if (sp_value == NULL)
+		return (0);
 	for (i = idx; ht->array[i]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
+			free(ht->array[i]->value)
 			ht->array[i]->value = cp_value;
 			return (1);
 		}
@@ -35,14 +37,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(cp_value);
 		return (0);
 	}
-	cp_key = strdup(key);
-	if (cp_key == NULL)
+	new->key = strdup(key);
+	if (new->key == NULL)
 	{
-		free(cp_value);
 		free(new);
 		return (0);
 	}
-	new->key = cp_key;
 	new->value = cp_value;
 	new->next = ht->array[idx];
 	ht->array[idx]->next = new;
