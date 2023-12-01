972f9b796166934751f1090b481c17566a3f43a7
abel-alx-github <abeltibeso323@gmail.com>
2023-12-01 10:27:08 -0800

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,20 +12,18 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new;
 	char *cp_value;
 
-	if (ht == NULL || *key == "\0" || key == NULL || value || key)
-	{
+	if (ht == NULL || *key == '\0' || key == NULL || value || key)
 		return (0);
-	}
 
 	idx = key_index((const unsigned char *) key, ht->size);
 	cp_value = strdup(value);
-	if (sp_value == NULL)
+	if (cp_value == NULL)
 		return (0);
 	for (i = idx; ht->array[i]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			free(ht->array[i]->value)
+			free(ht->array[i]->value);
 			ht->array[i]->value = cp_value;
 			return (1);
 		}
