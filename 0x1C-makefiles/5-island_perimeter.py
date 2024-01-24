aa576ed0e6aaab598241b2fe973dc36a57721207
abel-alx-github <abeltibeso323@gmail.com>
2024-01-24 12:42:40 -0800

    interview question

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,17 +3,26 @@
 
 
 def island_perimeter(grid):
-    """ return the primeter of an island area"""
-    p = 0
+    """Return the perimiter of an island.
+
+    The grid represents water by 0 and land by 1.
+
+    Args:
+        grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
+    """
+
+    perimeter = 0
     for row_idx, row in enumerate(grid):
         for elm_idx, elm in enumerate(row):
             if elm == 1:
                 if elm_idx > 0 and row[elm_idx - 1] == 0:
-                    p += 1
+                    perimeter += 1
                 if elm_idx > 0 and row[elm_idx + 1] == 0:
-                    p += 1
+                    perimeter += 1
                 if row_idx > 0 and grid[row_idx - 1][elm_idx] == 0:
-                    p += 1
+                    perimeter += 1
                 if row_idx > 0 and grid[row_idx + 1][elm_idx] == 0:
-                    p += 1
-    return p
+                    perimeter += 1
+    return perimeter
