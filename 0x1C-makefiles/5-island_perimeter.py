bcfd6ae77d66ac4f607a156959a6197c95137aa0
abel-alx-github <abeltibeso323@gmail.com>
2024-01-24 12:34:18 -0800

    interview question

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,12 +8,12 @@ def island_perimeter(grid):
     for row_idx, row in enumerate(grid):
         for elm_idx, elm in enumerate(row):
             if elm == 1:
-                if row[elm_idx - 1] == 0:
+                if elm_idx > 0 and row[elm_idx - 1] == 0:
                     p += 1
-                if row[elm_idx + 1] == 0:
+                if elm_idx > 0 and row[elm_idx + 1] == 0:
                     p += 1
-                if grid[row_idx - 1][elm_idx] == 0:
+                if row_idx > 0 and grid[row_idx - 1][elm_idx] == 0:
                     p += 1
-                if grid[row_idx + 1][elm_idx] == 0:
+                if row_idx > 0 and grid[row_idx + 1][elm_idx] == 0:
                     p += 1
     return p
