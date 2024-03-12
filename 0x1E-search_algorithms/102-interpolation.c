226dbc3d6673c6b0a5146d77e9e1c7b3af539fc4
abel-alx-github <abeltibeso323@gmail.com>
2024-03-12 13:14:06 -0700

    interpolation search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -8,9 +8,10 @@
 */
 int interpolation_search(int *array, size_t size, int value)
 {
-	int low, high;
-	size_t pos;
+	size_t low, high, pos;
 
+	if (array == NULL)
+		return (-1);
 	low = 0;
 	high = size - 1;
 
