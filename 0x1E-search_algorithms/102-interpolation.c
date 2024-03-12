6153fe6ed54713c81a5ae32c0ad8cff8b679c275
abel-alx-github <abeltibeso323@gmail.com>
2024-03-12 13:28:28 -0700

    interpolation search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -24,7 +24,8 @@ int interpolation_search(int *array, size_t size, int value)
 			pos = low + (((double)(high - low) / (array[high] - array[low])) *
 					(value - array[low]));
 			printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
-
+			if (pos > size)
+				return (-1);
 			if (array[pos] == value)
 				return (pos);
 			if (array[pos] < value)
