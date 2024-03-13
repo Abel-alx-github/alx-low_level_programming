41892e6cc9799707d57b6ca8ecae9b047af49d14
abel-alx-github <abeltibeso323@gmail.com>
2024-03-13 06:35:06 -0700

    linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -7,6 +7,7 @@
 *@size: size of arry
 *@value: int to search for
 *Return: indx at which the value is found or -1
+*Description : search the value in array and return its indx or -1
 */
 
 
@@ -14,7 +15,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	unsigned int index;
 
-	for (index = 0; array && size > 0 && index < size; index++)
+	for (index = 0; array && size != 0 && index < size; index++)
 	{
 		printf("Value checked array[%u] = [%d]\n", index, array[index]);
 		if (array[index] == value)
